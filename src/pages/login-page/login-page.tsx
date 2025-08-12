ac64f3e64411d3f4532aa36d4ccc331a75973d69
AleksandrMelexin <Melehin-Sasha7@yandex.ru>
2025-08-13 01:03:42 +0700

    переезд

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -5,6 +5,7 @@ const LoginPage = () => {
   return (
     <main className={styles.formWrapper}>
       <img src='icons/T1Logo.png' alt='logo' />
+      <img src='http://localhost:8080/uploads/achievements/4f526ab2-0474-4e53-8114-bd2b15cbb9a2.png'/>
       <div className={styles.formContent}>
         <LoginForm/>
       </div>
